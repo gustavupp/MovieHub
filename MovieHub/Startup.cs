9fd75ee073c7b5bf4dc16f6942e7f2fc7fed8bc6
Gustavo Pereira <xgustavux@hotmail.com>
2022-04-25 10:43:02 +1000

    Add project files.

---
+++ MovieHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieHub.Startup))]
+namespace MovieHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

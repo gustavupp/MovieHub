7540d53360f2c9adc8f3b73ac6f4b8d315e825c3
Gustavo Pereira <xgustavux@hotmail.com>
2022-04-25 14:56:53 +1000

    added viewModel and form

---
+++ MovieHub/Controllers/UpcomingMoviesController.cs
@@ -0,0 +1,28 @@
+﻿using MovieHub.Models;
+using MovieHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MovieHub.Controllers
+{
+    public class UpcomingMoviesController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public UpcomingMoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new UpcomingMovieViewModel()
+            {
+                MovieGenres = _context.MovieGenres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MovieHub/ViewModels/UpcomingMovieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MovieHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieHub.ViewModels
+{
+    public class UpcomingMovieViewModel
+    {
+        public int Id { get; set; }
+        public ApplicationUser Movie { get; set; }
+        public string ReleaseDate { get; set; }
+        public int RunningTime { get; set; }
+        public string Director { get; set; }
+        public MovieGenres MovieGenre { get; set; }
+        public IEnumerable<MovieGenres> MovieGenres { get; set; }
+    }
+}

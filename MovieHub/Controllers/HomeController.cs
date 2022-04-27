e602c86f230ccdc3db2a386d0fcdc6990fb3826c
Gustavo Pereira <xgustavux@hotmail.com>
2022-04-27 19:06:49 +1000

    added name to user

--- MovieHub/Controllers/HomeController.cs
+++ MovieHub/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MovieHub.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,16 @@ namespace MovieHub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingMoviesList = _context.UpcomingMovies.Include(m => m.AppUser).Where(m => m.ReleaseDate > DateTime.Now).ToList();
+            return View(upcomingMoviesList);
         }
 
     }

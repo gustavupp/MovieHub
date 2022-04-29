d348335cd0961cb4992c46fb3d477de3110fc821
Gustavo Pereira <xgustavux@hotmail.com>
2022-04-29 15:12:24 +1000

    worked on the api authorization and a few other things

--- MovieHub/Controllers/AccountController.cs
+++ MovieHub/Controllers/AccountController.cs
@@ -61,7 +61,7 @@ namespace MovieHub.Controllers
             return View();
         }
 
-        //
+        
         // POST: /Account/Login
         [HttpPost]
         [AllowAnonymous]
@@ -151,8 +151,14 @@ namespace MovieHub.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Name, Email = model.Email };
+                var user = new ApplicationUser { 
+                    UserName = model.Email, 
+                    Email = model.Email, 
+                    Name = model.Name 
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
+
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
---
+++ MovieHub/Controllers/AttendancesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Identity;
+using MovieHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+             _context = new ApplicationDbContext();
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int upcomingMovieId)
+        {
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Attendances.Any(a => a.UpcomingMovieId == upcomingMovieId && a.AttendeeId == userId);
+
+            if (exists)
+                return BadRequest("Attendance alaredy exists.");
+
+
+            var attendance = new Attendance()
+            {
+                UpcomingMovieId = upcomingMovieId,
+                AttendeeId = userId,
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok("Attendence included");
+        }
+
+        [HttpGet]
+        public IHttpActionResult test()
+        {
+            return Ok("all good");
+        }
+    }
+}

80b9b1e7ec3903398905b4648413c637a9134b6c
Gustavo Pereira <xgustavux@hotmail.com>
2022-05-21 12:06:36 +1000

    created notification/userNotification classes

---
+++ MovieHub/Models/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieHub.Models
+{
+    public class Notification
+    {
+        public int Id { get; private set; }
+
+        public DateTime DateTime { get; private set; }
+        public NotificationType NotificationType { get; private set; }
+        public DateTime? OriginalDateTime { get; set; }
+        public string OriginalVenue { get; set; }
+
+        [Required]
+        public UpcomingMovie UpcomingMovie { get; set; }
+    }
+}
---
+++ MovieHub/Models/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieHub.Models
+{
+    public enum NotificationType
+    {
+        UpcomingMovieCanceled = 1,
+        UpcomingMovieCreated = 2,
+        UpcomingMovieUpdated = 3,
+    }
+}

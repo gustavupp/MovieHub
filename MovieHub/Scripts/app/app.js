77a270daf3aa59296789a97830a8ffe838ae8954
Gustavo Pereira <xgustavux@hotmail.com>
2022-06-18 15:25:24 +1000

    fixes bundling issue

--- MovieHub/Scripts/app/app.js
+++ MovieHub/Scripts/app/app.js
@@ -1,1 +1,32 @@
 ﻿
+const initGigs = () => {
+    let notificationsBadge = document.querySelector('.notification-count');
+
+    function getData() {
+        let result = '';
+        fetch("/api/notifications")
+            .then(res => res.json())
+            .then(res => {
+                result = res;
+                notificationsBadge.textContent = res?.length;
+            })
+            .catch(err => new Error(`There was an error: ${err}`));
+        return result
+    }
+
+    function render(obj) {
+        if (obj) {
+            return `
+                       -> something here
+                    `
+        }
+    }
+
+    $('.notifications').popover({
+        html: true,
+        title: "notifications",
+        content: () => 'this is the content',
+        placement: 'bottom',
+        //template: '<div class="popover notification-count" role="tooltip">ADPAUdhAUhdPIAUhdPIUAHDS</div>'
+    });
+}
